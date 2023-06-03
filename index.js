dd45e61407ce30a3037a0ca4c5388f3ef562ce38
Karol <kpulawski13@gmail.com>
2023-06-03 17:06:16 +0100

    adjusted DisplayController

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const Gameboard = (() => {
-  const board = ["x", "x", "x", "x", "x", "x", "x", "x", "x"];
+  const board = ["", "", "", "", "", "", "", "", ""];
   return {
     board,
   };
 })();
 
 const DisplayController = (() => {
-  const renderBoard = (arr) => {
-    const board = document.querySelector(".game-board");
-    arr.forEach(function addTilesToBoard(element, index) {
-      const tile = document.createElement("div");
-      tile.classList.add("square");
-      tile.dataset.index = index;
-      tile.innerHTML = `${element}`;
-      board.appendChild(tile);
-    });
+  const _board = document.querySelector(".game-board");
+
+  function _addTilesToBoard(element, index) {
+    const tile = document.createElement("div");
+    tile.classList.add("square");
+    tile.dataset.index = index;
+    tile.innerHTML = `${element}`;
+    _board.appendChild(tile);
+  }
+
+  const renderBoardContent = (arr) => {
+    arr.forEach((e, i) => _addTilesToBoard(e, i));
   };
-  return { renderBoard };
+  return { renderBoardContent };
 })();
 
 const Player = () => {
   return {};
 };
 
-DisplayController.renderBoard(Gameboard.board);
+DisplayController.renderBoardContent(Gameboard.board);
