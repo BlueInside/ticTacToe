7d04563e73a201682d8aa20f2a0ff6fb874a9364
Karol <kpulawski13@gmail.com>
2023-06-03 16:51:36 +0100

    added func that render board

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const Gameboard = (() => {
-  const board = [];
-  return {};
+  const board = ["x", "x", "x", "x", "x", "x", "x", "x", "x"];
+  return {
+    board,
+  };
 })();
 
 const DisplayController = (() => {
-  return {};
+  const renderBoard = (arr) => {
+    const board = document.querySelector(".game-board");
+    arr.forEach(function addTilesToBoard(element, index) {
+      const tile = document.createElement("div");
+      tile.classList.add("square");
+      tile.dataset.index = index;
+      tile.innerHTML = `${element}`;
+      board.appendChild(tile);
+    });
+  };
+  return { renderBoard };
 })();
 
 const Player = () => {
   return {};
 };
+
+DisplayController.renderBoard(Gameboard.board);
