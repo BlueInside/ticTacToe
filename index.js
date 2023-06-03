ac0d0e83e10f57073f46ec3096da52fcd3f10303
Karol <kpulawski13@gmail.com>
2023-06-03 21:15:26 +0100

    moved some gameboardLogic into DisplayCOntroller

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
 const Gameboard = (() => {
-  const board = ["", "", "", "", "", "", "", "", ""];
+  const _board = ["", "", "", "", "", "", "", "", ""];
+  const markTile = (mark, index) => {
+    _board[index] = mark;
+  };
+  const getBoard = () => _board;
   return {
-    board,
+    markTile,
+    getBoard,
   };
 })();
 
 const DisplayController = (() => {
   const _board = document.querySelector(".game-board");
 
-  function _addTilesToBoard(element, index) {
+  let _tiles = null;
+
+  const _listenOnTiles = () => {
+    if (!_tiles) _tiles = document.querySelectorAll(".square");
+    _tiles.forEach((tile, index) =>
+      tile.addEventListener("click", function onTileClick(e) {
+        console.log(index);
+      })
+    );
+  };
+  function _populateBoard(element, index) {
     const tile = document.createElement("div");
     tile.classList.add("square");
     tile.dataset.index = index;
     tile.innerHTML = `${element}`;
     _board.appendChild(tile);
   }
-
-  const renderBoardContent = (arr) => {
-    arr.forEach((e, i) => _addTilesToBoard(e, i));
+  const setUpBoard = () => {
+    renderBoardContent(Gameboard.getBoard());
+    _listenOnTiles();
   };
-  return { renderBoardContent };
+  const renderBoardContent = (arr) => {
+    arr.forEach((e, i) => _populateBoard(e, i));
+  };
+  return { renderBoardContent, setUpBoard };
 })();
 
-const Player = () => {
-  return {};
+const Player = (mark, name) => {
+  return { mark, name };
 };
 
-DisplayController.renderBoardContent(Gameboard.board);
+const Game = (() => {
+  const _player1 = Player("X", "player1");
+  const _player2 = Player("0", "player2");
+
+  const playRound = [];
+  return { playRound };
+})();
+
+DisplayController.setUpBoard();
